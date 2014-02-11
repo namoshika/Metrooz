06d2a901e2fd8a556056395fedda4e81c5d5d45a
namoshika <namoshika@users.noreply.github.com>
2014-02-12 00:50:23 +0900

    画像キャッシュをLRUとして保持するようにした

--- GPlusBrowser/ViewModel/DataCacheDictionary.cs
+++ GPlusBrowser/ViewModel/DataCacheDictionary.cs
@@ -12,79 +12,52 @@ namespace GPlusBrowser
 {
     public static class DataCacheDictionary
     {
-        static DataCacheDictionary() { System.Net.ServicePointManager.DefaultConnectionLimit = 8; }
+        static DataCacheDictionary() { System.Net.ServicePointManager.DefaultConnectionLimit = 16; }
         static DateTime _lastCleanupTime = DateTime.UtcNow;
         static readonly HttpClient _defaultHttpClient = new HttpClient();
-        static readonly Dictionary<Uri, WeakReference<BitmapImage>> _imgCacheDictionary = new Dictionary<Uri, WeakReference<BitmapImage>>();
-        static readonly Dictionary<Uri, Task<BitmapImage>> _imgJobDictionary = new Dictionary<Uri, Task<BitmapImage>>();
-        static readonly System.Threading.SemaphoreSlim _syncer = new System.Threading.SemaphoreSlim(1, 1);
+        static readonly CacheDictionary<Uri, ImageCache, Task<BitmapImage>> _imgCacheDictionary = new CacheDictionary<Uri, ImageCache, Task<BitmapImage>>(90, 30, false, tsk => new ImageCache() { Value = tsk });
 
-        public async static Task<BitmapImage> DownloadImage(Uri imageUrl, HttpClient client = null)
+        public static Task<BitmapImage> DownloadImage(Uri imageUrl, HttpClient client = null)
         {
             if (imageUrl == null)
-                return null;
-            try
-            {
-                await _syncer.WaitAsync();
-
-                Task<BitmapImage> aa;
-                if (_imgJobDictionary.TryGetValue(imageUrl, out aa))
-                    return await aa;
-                else
-                    return await Task.Run(async () =>
+                return Task.FromResult((BitmapImage)null);
+            client = client ?? _defaultHttpClient;
+            return _imgCacheDictionary.Update(imageUrl, () => PrivateDownloadImage(imageUrl, client)).Value;
+        }
+        static Task<BitmapImage> PrivateDownloadImage(Uri imageUrl, HttpClient client = null)
+        {
+            return Task.Run(async () =>
+                {
+                    try
                     {
-                        try
+                        BitmapImage img;
+                        using (var mStrm = new System.IO.MemoryStream())
                         {
-                            WeakReference<BitmapImage> imgRef;
-                            BitmapImage img;
-                            bool isNewItem = true;
+                            client = client ?? _defaultHttpClient;
+                            int recieveByte;
+                            byte[] buff = new byte[1024];
+                            using (var strm = await client.GetStreamAsync(imageUrl))
+                                while ((recieveByte = await strm.ReadAsync(buff, 0, buff.Length)) > 0)
+                                    mStrm.Write(buff, 0, recieveByte);
+                            mStrm.Seek(0, System.IO.SeekOrigin.Begin);
 
-                            if (_imgCacheDictionary.TryGetValue(imageUrl, out imgRef) == false || (isNewItem = imgRef.TryGetTarget(out img)) == false)
-                                using (var mStrm = new System.IO.MemoryStream())
-                                {
-                                    client = client ?? _defaultHttpClient;
-                                    int recieveByte;
-                                    byte[] buff = new byte[1024];
-                                    using (var strm = await client.GetStreamAsync(imageUrl))
-                                        while ((recieveByte = await strm.ReadAsync(buff, 0, buff.Length)) > 0)
-                                            mStrm.Write(buff, 0, recieveByte);
-                                    mStrm.Seek(0, System.IO.SeekOrigin.Begin);
+                            img = new BitmapImage();
+                            img.BeginInit();
+                            img.CacheOption = BitmapCacheOption.OnLoad;
+                            img.CreateOptions = BitmapCreateOptions.None;
+                            img.StreamSource = mStrm;
+                            img.EndInit();
+                            img.Freeze();
 
-                                    img = new BitmapImage();
-                                    img.BeginInit();
-                                    img.CacheOption = BitmapCacheOption.OnLoad;
-                                    img.CreateOptions = BitmapCreateOptions.None;
-                                    img.StreamSource = mStrm;
-                                    img.EndInit();
-                                    img.Freeze();
-
-                                    if (isNewItem)
-                                        _imgCacheDictionary.Add(imageUrl, new WeakReference<BitmapImage>(img));
-                                    else
-                                        imgRef.SetTarget(img);
-                                }
-                            if (DateTime.UtcNow - _lastCleanupTime > TimeSpan.FromMinutes(30))
-                            {
-                                _lastCleanupTime = DateTime.UtcNow;
-                                GC();
-                            }
                             return img;
                         }
-                        catch (System.Net.Http.HttpRequestException) { return null; }
-                        catch (NotSupportedException) { return null; }
-                    });
-            }
-            finally
-            { _syncer.Release(); }
-        }
-        public static void GC()
-        {
-            BitmapImage tmp;
-            foreach (var item in _imgCacheDictionary.ToArray())
-            {
-                if (item.Value.TryGetTarget(out tmp) == false)
-                    _imgCacheDictionary.Remove(item.Key);
-            }
+                    }
+                    catch (System.Net.Http.HttpRequestException) { return null; }
+                    catch (NotSupportedException) { return null; }
+                });
         }
     }
+    public class ImageCache : ICacheInfo<Task<BitmapImage>>
+    { public Task<BitmapImage> Value { get; set; } }
+
 }
