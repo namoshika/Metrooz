ea66ee4472ec8d77311e50df57bc54a4e1f0fc54
namoshika <namoshika@users.noreply.github.com>
2014-04-30 16:36:58 +0900

    Activityのコメントの開閉をScrollViewerを用いたもので再実装
    既存の仕組みを出来る限り活用したものに差し替えたかった。

--- Metrooz/Controls/ExpandableListView.cs
+++ Metrooz/Controls/ExpandableListView.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -15,163 +16,194 @@ using System.Windows.Shapes;
 
 namespace Metrooz.Controls
 {
-    /// <summary>
-    /// ExpandableListView.xaml の相互作用ロジック
-    /// </summary>
-    public partial class ExpandableListView : HeaderedItemsControl
+    public class ExpandableListView : ItemsControl
     {
         public ExpandableListView()
         {
-            _expandAnimationDuration = _insertAnimationDuration = (Duration)TimeSpan.FromMilliseconds(100);
-            _displayMaxCount = 2;
-            ShiftTranslater = new TranslateTransform();
-            Loaded += ExpandableListView_Loaded;
+            _miniAreaItemGetter = isExpanded => ItemContainerGenerator.Items
+                .Skip(isExpanded ? 0 : Math.Max(ItemContainerGenerator.Items.Count - 2, 0))
+                .Select(item => ItemContainerGenerator.ContainerFromItem(item))
+                .Cast<FrameworkElement>();
         }
         static ExpandableListView()
+        { DefaultStyleKeyProperty.OverrideMetadata(typeof(ExpandableListView), new FrameworkPropertyMetadata(typeof(ExpandableListView))); }
+        readonly static TimeSpan _insertAnimeDuration = TimeSpan.FromMilliseconds(100);
+        readonly static TimeSpan _expandAnimeDuration = TimeSpan.FromMilliseconds(300);
+        readonly Func<bool, IEnumerable<FrameworkElement>> _miniAreaItemGetter;
+        int _activeAnimeCount;
+        //表示領域調整用のスクロール量
+        ScrollViewer _scrollViewer;
+        //_scrollViewer.ExtentHeightではタイミングなどで上手く欲しい値が取得できなかった。
+        //それが再現性の低いバグを生じさせていた。そのため、FullHeightとして自前のを用意する。
+        public double FullHeight
         {
-            DefaultStyleKeyProperty.OverrideMetadata(
-                typeof(ExpandableListView), new FrameworkPropertyMetadata(typeof(ExpandableListView)));
+            get { return (double)GetValue(FullHeightProperty); }
+            set { SetValue(FullHeightProperty, value); }
         }
-        FrameworkElement _itemContainer;
-        StackPanel _itemsPanel;
-        Duration _expandAnimationDuration;
-        Duration _insertAnimationDuration;
-        int _displayMaxCount;
-
-        public bool IsExpand
+        public double MiniHeight
         {
-            get { return (bool)GetValue(IsExpandProperty); }
-            set { SetValue(IsExpandProperty, value); }
+            get { return (double)GetValue(MiniHeightProperty); }
+            set { SetValue(MiniHeightProperty, value); }
         }
-        public bool Expandable
+        public bool IsExpanded
         {
-            get { return (bool)GetValue(ExpandableProperty); }
-            set { SetValue(ExpandableProperty, value); }
+            get { return (bool)GetValue(IsExpandedProperty); }
+            set { SetValue(IsExpandedProperty, value); }
         }
         public double ScrollOffset
         {
             get { return (double)GetValue(ScrollOffsetProperty); }
             set { SetValue(ScrollOffsetProperty, value); }
         }
-        public TranslateTransform ShiftTranslater
+
+        public override void OnApplyTemplate()
         {
-            get { return (TranslateTransform)GetValue(ShiftTranslaterProperty); }
-            set { SetValue(ShiftTranslaterProperty, value); }
+            base.OnApplyTemplate();
+            _scrollViewer = (ScrollViewer)Template.FindName("scrollViewer", this);
         }
-        protected override void OnItemsChanged(System.Collections.Specialized.NotifyCollectionChangedEventArgs e)
+        protected override Size ArrangeOverride(Size arrangeBounds)
+        {
+            if (_activeAnimeCount == 0)
+                BeginAnimation(ScrollOffsetProperty,
+                    new DoubleAnimation(IsExpanded ? 0.0 : FullHeight - MiniHeight, TimeSpan.Zero, FillBehavior.HoldEnd),
+                    HandoffBehavior.SnapshotAndReplace);
+            return base.ArrangeOverride(new Size(
+                arrangeBounds.Width, double.IsNaN(Height) ? (IsExpanded ? FullHeight : MiniHeight) : Height));
+        }
+        protected override Size MeasureOverride(Size constraint)
+        {
+            //縮小モード時も最後の要素2個を表示する
+            var desireHeight = IsExpanded ? FullHeight : MiniHeight;
+            if (_activeAnimeCount == 0)
+                BeginAnimation(ScrollOffsetProperty,
+                    new DoubleAnimation(IsExpanded ? 0.0 : FullHeight - MiniHeight, TimeSpan.Zero, FillBehavior.HoldEnd),
+                    HandoffBehavior.SnapshotAndReplace);
+            return base.MeasureOverride(new Size(constraint.Width, double.IsNaN(Height) ? desireHeight : Height));
+        }
+        protected async override void OnItemsChanged(System.Collections.Specialized.NotifyCollectionChangedEventArgs e)
         {
             base.OnItemsChanged(e);
-            Expandable = Items.Count > 2;
-
             switch (e.Action)
             {
                 case System.Collections.Specialized.NotifyCollectionChangedAction.Add:
-                    Refresh(true);
+                    var oldMiniHeight = ActualHeight;
+                    var newMiniHeight = _miniAreaItemGetter(false)
+                        .Select(container =>
+                            {
+                                container.UpdateLayout();
+                                return container.ActualHeight;
+                            })
+                        .Sum();
+                    var newFullHeight = _miniAreaItemGetter(true)
+                        .Select(container => container.ActualHeight).Sum();
+
+                    //高さと表示領域調整用のスクロール量を調整
+                    //IsExpandedがtrueの時は常にScrollOffsetはtrueなのでスルー
+                    System.Threading.Interlocked.Increment(ref _activeAnimeCount);
+                    var heightTL = new DoubleAnimationUsingKeyFrames() { FillBehavior = FillBehavior.HoldEnd, AccelerationRatio = 0.0, DecelerationRatio = 1.0, };
+                    heightTL.KeyFrames.Add(new LinearDoubleKeyFrame(oldMiniHeight, TimeSpan.FromMilliseconds(0)));
+                    heightTL.KeyFrames.Add(new LinearDoubleKeyFrame(IsExpanded ? newFullHeight : newMiniHeight, _insertAnimeDuration));
+                    heightTL.KeyFrames.Add(new DiscreteDoubleKeyFrame(double.NaN, _insertAnimeDuration));
+                    BeginAnimation(HeightProperty, heightTL, HandoffBehavior.SnapshotAndReplace);
+                    if (IsExpanded == false)
+                        BeginAnimation(ScrollOffsetProperty, new DoubleAnimation(
+                            newFullHeight - newMiniHeight, _insertAnimeDuration) { FillBehavior = FillBehavior.HoldEnd, AccelerationRatio = 0.0, DecelerationRatio = 1.0, },
+                            HandoffBehavior.SnapshotAndReplace);
+                    //アニメ終了後
+                    await Task.Delay(_insertAnimeDuration);
+                    System.Threading.Interlocked.Decrement(ref _activeAnimeCount);
                     break;
-                case System.Collections.Specialized.NotifyCollectionChangedAction.Remove:
-                case System.Collections.Specialized.NotifyCollectionChangedAction.Reset:
-                    Refresh(false);
-                    break;
             }
-        }
-        protected override void PrepareContainerForItemOverride(DependencyObject element, object item)
-        {
-            base.PrepareContainerForItemOverride(element, item);
-            var container = (FrameworkElement)element;
-            container.SizeChanged += itemPresenter_SizeChanged;
-        }
-        protected override void ClearContainerForItemOverride(DependencyObject element, object item)
-        {
-            base.ClearContainerForItemOverride(element, item);
-            var container = (FrameworkElement)element;
-            container.SizeChanged -= itemPresenter_SizeChanged;
-        }
-        void ExpandableListView_Loaded(object sender, RoutedEventArgs e)
-        {
-            _itemContainer = (FrameworkElement)Template.FindName("PART_scroller_itemsPresenter", this);
-            _itemsPanel = (StackPanel)
-                VisualTreeHelper.GetChild(
-                VisualTreeHelper.GetChild(_itemContainer, 0), 0);
-            Refresh(false);
-        }
-        void itemPresenter_SizeChanged(object sender, SizeChangedEventArgs e)
-        {
-            if (_itemContainer == null)
-                return;
-            Refresh(false);
-        }
-        void Refresh(bool isEnableAnimation)
-        {
-            if (_itemsPanel == null)
-                return;
-
-            var newAreaHeight = 0.0;
-            for (var i = IsExpand ? 0 : Math.Max(Items.Count - _displayMaxCount, 0); i < Items.Count; i++)
-            {
-                var container = (FrameworkElement)ItemContainerGenerator.ContainerFromIndex(i);
-                if (container.IsMeasureValid == false)
-                    container.UpdateLayout();
-                newAreaHeight += container.ActualHeight;
-            }
-            if (isEnableAnimation)
-            {
-                //スクロール
-                var scrollAnime = new DoubleAnimation(newAreaHeight - _itemsPanel.ActualHeight, _insertAnimationDuration);
-                var heightAnime = new DoubleAnimation(newAreaHeight, _insertAnimationDuration);
-                BeginAnimation(ScrollOffsetProperty, scrollAnime, HandoffBehavior.SnapshotAndReplace);
-                _itemContainer.BeginAnimation(HeightProperty, heightAnime, HandoffBehavior.SnapshotAndReplace);
-            }
-            else
-            {
-                var scrollAnime = new DoubleAnimation(newAreaHeight - _itemsPanel.ActualHeight, TimeSpan.Zero);
-                var heightAnime = new DoubleAnimation(newAreaHeight, TimeSpan.Zero);
-                BeginAnimation(ScrollOffsetProperty, scrollAnime, HandoffBehavior.SnapshotAndReplace);
-                _itemContainer.BeginAnimation(HeightProperty, heightAnime, HandoffBehavior.SnapshotAndReplace);
-            }
+            InvalidateMeasure();
+            InvalidateArrange();
         }
 
-        public static readonly DependencyProperty IsExpandProperty = DependencyProperty.Register(
-            "IsExpand", typeof(bool), typeof(ExpandableListView), new FrameworkPropertyMetadata(false, Changed_IsExpand));
-        public static readonly DependencyProperty ExpandableProperty = DependencyProperty.Register(
-            "Expandable", typeof(bool), typeof(ExpandableListView), new PropertyMetadata(true));
-        public static readonly DependencyProperty ScrollOffsetProperty = DependencyProperty.Register(
-            "ScrollOffset", typeof(double), typeof(ExpandableListView), new PropertyMetadata(0.0, Changed_ScrollOffset));
-        public static readonly DependencyProperty ShiftTranslaterProperty = DependencyProperty.Register(
-            "ShiftTranslater", typeof(TranslateTransform), typeof(ExpandableListView));
-
-        static void Changed_IsExpand(DependencyObject sender, DependencyPropertyChangedEventArgs e)
-        {
-            var target = (ExpandableListView)sender;
-            var duration = (Duration)TimeSpan.FromMilliseconds(500);
-
-            if ((bool)e.NewValue)
-            {
-                var heightAnime = new DoubleAnimation(target._itemsPanel.ActualHeight, duration) { DecelerationRatio = 1.0 };
-                var scrollAnime = new DoubleAnimation(0.0, duration) { DecelerationRatio = 1.0 };
-                target._itemContainer.BeginAnimation(HeightProperty, heightAnime, HandoffBehavior.SnapshotAndReplace);
-                target.BeginAnimation(ScrollOffsetProperty, scrollAnime, HandoffBehavior.SnapshotAndReplace);
-            }
-            else
-            {
-                var newAreaHeight = 0.0;
-                for (var i = target.IsExpand ? 0 : Math.Max(target.Items.Count - target._displayMaxCount, 0); i < target.Items.Count; i++)
+        public static readonly DependencyProperty IsExpandedProperty = DependencyProperty.Register(
+            "IsExpanded", typeof(bool), typeof(ExpandableListView), new UIPropertyMetadata(false, async (sender, e) =>
                 {
-                    var container = (FrameworkElement)target.ItemContainerGenerator.ContainerFromIndex(i);
-                    newAreaHeight += container.ActualHeight;
-                }
-                var heightAnime = new DoubleAnimation(newAreaHeight, duration) { DecelerationRatio = 1.0 };
-                var scrollAnime = new DoubleAnimation(newAreaHeight - target._itemsPanel.ActualHeight, duration) { DecelerationRatio = 1.0 };
-                target._itemContainer.BeginAnimation(HeightProperty, heightAnime, HandoffBehavior.SnapshotAndReplace);
-                target.BeginAnimation(ScrollOffsetProperty, scrollAnime, HandoffBehavior.SnapshotAndReplace);
-            }
-        }
-        static void Changed_ScrollOffset(DependencyObject sender, DependencyPropertyChangedEventArgs e)
-        {
-            var mediator = (ExpandableListView)sender;
-            mediator.ShiftTranslater.Y = (double)e.NewValue;
-        }
+                    var self = (ExpandableListView)sender;
+                    System.Threading.Interlocked.Increment(ref self._activeAnimeCount);
+                    var oldMiniHeight = self.ActualHeight;
+                    var newMiniHeight = (bool)e.NewValue ? self.FullHeight : self.MiniHeight;
+                    var heightTL = new DoubleAnimationUsingKeyFrames() { FillBehavior = FillBehavior.HoldEnd, AccelerationRatio = 0.0, DecelerationRatio = 1.0, };
+                    heightTL.KeyFrames.Add(new LinearDoubleKeyFrame(oldMiniHeight, TimeSpan.FromMilliseconds(0)));
+                    heightTL.KeyFrames.Add(new LinearDoubleKeyFrame(newMiniHeight, _expandAnimeDuration));
+                    heightTL.KeyFrames.Add(new DiscreteDoubleKeyFrame(double.NaN, _expandAnimeDuration));
+                    self.BeginAnimation(HeightProperty, heightTL, HandoffBehavior.SnapshotAndReplace);
+                    self.BeginAnimation(ScrollOffsetProperty, new DoubleAnimation(
+                        (bool)e.NewValue ? 0.0 : self.FullHeight - self.MiniHeight, _expandAnimeDuration) { FillBehavior = FillBehavior.HoldEnd, AccelerationRatio = 0.0, DecelerationRatio = 1.0, },
+                        HandoffBehavior.SnapshotAndReplace);
+                    //アニメ終了後
+                    await Task.Delay(_expandAnimeDuration);
+                    System.Threading.Interlocked.Decrement(ref self._activeAnimeCount);
+                    self.InvalidateMeasure();
+                    self.InvalidateArrange();
+                }));
+        public static readonly DependencyProperty MiniHeightProperty = DependencyProperty.Register(
+            "MiniHeight", typeof(double), typeof(ExpandableListView), new FrameworkPropertyMetadata(0.0, FrameworkPropertyMetadataOptions.AffectsMeasure | FrameworkPropertyMetadataOptions.AffectsArrange));
+        public static readonly DependencyProperty FullHeightProperty = DependencyProperty.Register(
+            "FullHeight", typeof(double), typeof(ExpandableListView), new FrameworkPropertyMetadata(0.0, FrameworkPropertyMetadataOptions.AffectsMeasure | FrameworkPropertyMetadataOptions.AffectsArrange));
+        public static readonly DependencyProperty ScrollOffsetProperty = DependencyProperty.Register(
+            "ScrollOffset", typeof(double), typeof(ExpandableListView), new FrameworkPropertyMetadata(0.0, (sender, e) =>
+                {
+                    var self = (ExpandableListView)sender;
+                    if (self._scrollViewer == null)
+                        return;
+                    self._scrollViewer.ScrollToVerticalOffset(Math.Max((double)e.NewValue, 0));
+                }));
     }
+    public class StackPanelEx : StackPanel
+    {
+        public StackPanelEx()
+        {
+            _miniAreaItemGetter = isExpanded =>
+            {
+                var startIdx = isExpanded ? 0 : Math.Max(InternalChildren.Count - 2, 0);
+                return Enumerable
+                    .Range(startIdx, InternalChildren.Count - startIdx)
+                    .Select(idx => InternalChildren[idx])
+                    .Cast<FrameworkElement>();
+            };
+            SizeChanged += StackPanelEx_SizeChanged;
+        }
+        readonly Func<bool, IEnumerable<FrameworkElement>> _miniAreaItemGetter;
+        /// <summary>
+        /// Childrenの最後の2件のみを表示した際の高さ
+        /// </summary>
+        public double MiniHeight
+        {
+            get { return (double)GetValue(MiniHeightProperty); }
+            set { SetValue(MiniHeightProperty, value); }
+        }
+        /// <summary>
+        /// Childrenの全てを表示した際の高さ
+        /// </summary>
+        public double FullHeight
+        {
+            get { return (double)GetValue(FullHeightProperty); }
+            set { SetValue(FullHeightProperty, value); }
+        }
 
+        protected override Size ArrangeOverride(Size arrangeSize)
+        {
+            var res = base.ArrangeOverride(arrangeSize);
+            MiniHeight = _miniAreaItemGetter(false).Select(ele => ele.ActualHeight).Sum();
+            return res;
+        }
+        protected override Size MeasureOverride(Size constraint)
+        {
+            var res = base.MeasureOverride(constraint);
+            MiniHeight = _miniAreaItemGetter(false).Select(ele => ele.DesiredSize.Height).Sum();
+            return res;
+        }
+        void StackPanelEx_SizeChanged(object sender, SizeChangedEventArgs e)
+        { FullHeight = ActualHeight; }
+
+        public static readonly DependencyProperty MiniHeightProperty = DependencyProperty.Register(
+            "MiniHeight", typeof(double), typeof(StackPanelEx), new FrameworkPropertyMetadata(0.0));
+        public static readonly DependencyProperty FullHeightProperty = DependencyProperty.Register(
+            "FullHeight", typeof(double), typeof(StackPanelEx), new FrameworkPropertyMetadata(0.0));
+    }
     public class IsExpandCommentCountToStringConverter : IMultiValueConverter
     {
         public object Convert(object[] values, Type targetType, object parameter, System.Globalization.CultureInfo culture)
