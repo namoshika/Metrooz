8f6e4d01d1bc452b9dd5714cb3f42ff3a865e335
namoshika <namoshika@users.noreply.github.com>
2014-05-17 19:40:35 +0900

    ストリーム休止機能でアクティビティのコメントが更新されなくなるバグを修正。第2弾
    1. 7e7edbcf0ca67fbff32f5b7524507eb9475935dd のバグ修正
    一時停止モード時にあふれたActivityをDispose()した場合、表示されているActivityの更新が停止してしまっていた。そのため、表示されているActivityへは手出ししないようにした。

--- Metrooz/Model/Stream/Stream.cs
+++ Metrooz/Model/Stream/Stream.cs
@@ -87,48 +87,7 @@ namespace Metrooz.Model
 
                         //受信開始
                         Status = StreamStateType.Connected;
-                        _streamObj = Circle.GetStream().Subscribe(async newInfo =>
-                            {
-                                try
-                                {
-                                    await _syncer.WaitAsync().ConfigureAwait(false);
-                                    //一時停止している場合は外から見えない領域でストリームを更新
-                                    //するために_activitiesではなく_tmpActivitiesが更新される
-                                    var innerActiveActivities = _isPause ? _hiddenActivities : _activities;
-                                    var item = innerActiveActivities.FirstOrDefault(activity => activity.CoreInfo.Id == newInfo.Id);
-                                    var existUpdate = false;
-                                    switch (newInfo.PostStatus)
-                                    {
-                                        case PostStatusType.First:
-                                        case PostStatusType.Edited:
-                                            //itemがnullの場合は更新する。nullでない場合はすでにある値を更新する。
-                                            //しかし更新はActivityオブジェクト自体が行うため、Streamでは行わない
-                                            if (item == null)
-                                            {
-                                                existUpdate = true;
-                                                item = new Activity(newInfo);
-                                                innerActiveActivities.Insert(0, item);
-                                                if (innerActiveActivities.Count > MAX_ACTIVITIES_COUNT)
-                                                {
-                                                    innerActiveActivities[MAX_ACTIVITIES_COUNT].Dispose();
-                                                    innerActiveActivities.RemoveAt(MAX_ACTIVITIES_COUNT);
-                                                }
-                                            }
-                                            break;
-                                        case PostStatusType.Removed:
-                                            innerActiveActivities.Remove(item);
-                                            existUpdate = true;
-                                            break;
-                                    }
-                                    if (_isPause && existUpdate)
-                                    {
-                                        Status = StreamStateType.Paused;
-                                        ChangedActivityCount++;
-                                    }
-                                }
-                                finally
-                                { _syncer.Release(); }
-                            },
+                        _streamObj = Circle.GetStream().Subscribe(RecievedActivity,
                             async ex =>
                             {
                                 try
@@ -163,7 +122,10 @@ namespace Metrooz.Model
                                 else if (aIdx != hIdx)
                                     _activities.Move(aIdx, hIdx);
                             for (aIdx = _activities.Count - 1; aIdx >= hIdx; aIdx--)
+                            {
+                                _activities[aIdx].Dispose();
                                 _activities.RemoveAt(aIdx);
+                            }
                         }
                         //一時停止が終わったらActivityのGC解放のためにも消す
                         _hiddenActivities = null;
@@ -202,6 +164,52 @@ namespace Metrooz.Model
                     _streamObj.Dispose();
                 foreach (var item in _activities)
                     item.Dispose();
+            }
+            finally
+            { _syncer.Release(); }
+        }
+        async void RecievedActivity(ActivityInfo newInfo)
+        {
+            try
+            {
+                await _syncer.WaitAsync().ConfigureAwait(false);
+                //一時停止している場合は外から見えない領域でストリームを更新
+                //するために_activitiesではなく_tmpActivitiesが更新される
+                var innerActiveActivities = _isPause ? _hiddenActivities : _activities;
+                var item = innerActiveActivities.FirstOrDefault(activity => activity.CoreInfo.Id == newInfo.Id);
+                var existUpdate = false;
+                switch (newInfo.PostStatus)
+                {
+                    case PostStatusType.First:
+                    case PostStatusType.Edited:
+                        //itemがnullの場合は更新する。nullでない場合はすでにある値を更新する。
+                        //しかし更新はActivityオブジェクト自体が行うため、Streamでは行わない
+                        if (item == null)
+                        {
+                            existUpdate = true;
+                            item = new Activity(newInfo);
+                            innerActiveActivities.Insert(0, item);
+                            if (innerActiveActivities.Count > MAX_ACTIVITIES_COUNT)
+                            {
+                                var activity = innerActiveActivities[MAX_ACTIVITIES_COUNT];
+                                //一時停止モード時の古いActivityのDispose()は表示されているActivityの更新を誤停止する可能性があるため、表示されているActivityと照合してDispose()する。
+                                //この時、_isPauseはスレッドロック関係でinnerActiveActivitiesの状態と連動する保証がない。そのため、以下の条件文となっている。
+                                if (innerActiveActivities == _hiddenActivities && _activities.Contains(activity) == false)
+                                    innerActiveActivities[MAX_ACTIVITIES_COUNT].Dispose();
+                                innerActiveActivities.RemoveAt(MAX_ACTIVITIES_COUNT);
+                            }
+                        }
+                        break;
+                    case PostStatusType.Removed:
+                        innerActiveActivities.Remove(item);
+                        existUpdate = true;
+                        break;
+                }
+                if (_isPause && existUpdate)
+                {
+                    Status = StreamStateType.Paused;
+                    ChangedActivityCount++;
+                }
             }
             finally
             { _syncer.Release(); }
