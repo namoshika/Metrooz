ae97551717229a1f73c9d88dff5e86bbbdcf3854
namoshika <namoshika@users.noreply.github.com>
2014-06-26 07:55:07 +0900

    成果物をzip圧縮してpublishフォルダに置く仕組みを作った

---
+++ Metrooz/Publish.fsx
@@ -0,0 +1,36 @@
+﻿#r "System.IO.Compression"
+open System.IO
+open System.IO.Compression
+
+let isReleaseBuild = fsi.CommandLineArgs.[2] = "Release"
+if isReleaseBuild then
+    let prjDir = __SOURCE_DIRECTORY__
+    let binDir = prjDir + "\\bin\\Release"
+    let pubDir = prjDir + "\\Publish"
+    let appName = "Metrooz"
+
+    //Cleaning Dir
+    let pdbAndXmlFiles =
+        [yield! Directory.EnumerateFiles(binDir, "*.dll", SearchOption.AllDirectories)
+         yield! Directory.EnumerateFiles(binDir, "*.exe", SearchOption.AllDirectories)]
+        |> Seq.map(fun path -> path.Substring(0, path.Length - Path.GetExtension(path).Length))
+        |> Seq.collect(fun path -> [".pdb"; ".xml"] |> Seq.map(fun ex -> path + ex))
+        |> Seq.where(fun path -> File.Exists(path))
+    for filePath in pdbAndXmlFiles do
+        File.Delete(filePath)
+
+    //Publish
+    let rec dirToZip zipPath targetDirPath targetFilePaths=
+        use zipStrm = File.Create(zipPath)
+        use zipArch = new ZipArchive(zipStrm, ZipArchiveMode.Create)
+        targetFilePaths |> Seq.iter(fun path ->
+            use fileStrm = File.OpenRead(path)
+            let entryPath = path.Substring((targetDirPath + Path.DirectorySeparatorChar.ToString()).Length)
+            let entry = zipArch.CreateEntry(entryPath)
+            use entryStrm = entry.Open()
+            fileStrm.CopyTo(entryStrm)
+        )
+    ignore(Directory.CreateDirectory(pubDir))
+    Directory.EnumerateFiles(binDir, "*", SearchOption.AllDirectories)
+    |> Seq.where(fun path -> path.Contains(".vshost.exe") = false)
+    |> dirToZip (pubDir + "\\" + appName + ".zip") binDir
